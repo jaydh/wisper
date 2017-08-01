7c61aef914baa63b50ad4a236aa15f9957375b20
Jay Howard <jaydh@uw.edu>
2017-08-01 03:46:11 -0700

    Fixed import

--- src/actions/syncArticles.ts
+++ src/actions/syncArticles.ts
@@ -1,37 +1,9 @@
 import { auth, database } from '../firebase';
-import * as constants from '../constants/actionTypes';
+// import * as constants from '../constants/actionTypes';
 import { Dispatch } from 'react-redux';
 import { Article as articleType } from '../constants/StoreState';
-export interface SyncArticlesRequested {
-  type: constants.SYNC_ARTICLES_REQUESTED;
-}
 
-export interface SyncArticlesFulfilled {
-  type: constants.SYNC_ARTICLES_FULFILLED;
-  articles: articleType[];
-}
-export interface SyncArticlesRejected {
-  type: constants.SYNC_ARTICLES_REJECTED;
-}
-
-function SyncArticlesRequested(): SyncArticlesRequested {
-  return {
-    type: constants.SYNC_ARTICLES_REQUESTED
-  };
-}
-
-function SyncArticlesRejected(): SyncArticlesRejected {
-  return {
-    type: constants.SYNC_ARTICLES_REJECTED
-  };
-}
-
-function SyncArticlesFulfilled(articles: articleType[]): SyncArticlesFulfilled {
-  return {
-    type: constants.SYNC_ARTICLES_FULFILLED,
-    articles: articles
-  };
-}
+// todo: add types
 
 function UpdateArticle(article: articleType) {
   return {
