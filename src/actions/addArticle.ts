13c8ff2ea2e04a7dbdec08056510a5a2cbf1024a
Jay Howard <jaydh@uw.edu>
2017-08-01 03:38:29 -0700

    detached listener for when adding article from client

--- src/actions/addArticle.ts
+++ src/actions/addArticle.ts
@@ -1,80 +1,89 @@
 import * as constants from '../constants/actionTypes';
 import { auth, database } from '../firebase';
 import { Dispatch } from 'react-redux';
+import { ListenToFirebase } from './syncArticles';
 let Hashes = require('jshashes');
 
 var SHA1 = new Hashes.SHA1();
 const now = new Date();
 
 export interface AddArticleRequested {
-    type: constants.ADD_ARTICLE_REQUESTED;
+  type: constants.ADD_ARTICLE_REQUESTED;
 }
 export interface AddArticleFulfilled {
-    type: constants.ADD_ARTICLE_FULFILLED;
-    articleLink: string;
-    articleHash: string;
+  type: constants.ADD_ARTICLE_FULFILLED;
+  articleLink: string;
+  articleHash: string;
 }
 export interface AddArticleRejected {
-    type: constants.ADD_ARTICLE_REJECTED;
+  type: constants.ADD_ARTICLE_REJECTED;
 }
-export type AddArticleActions = AddArticleFulfilled | AddArticleRejected | AddArticleRequested;
+export type AddArticleActions =
+  | AddArticleFulfilled
+  | AddArticleRejected
+  | AddArticleRequested;
 
 function AddArticleRequested(): AddArticleRequested {
-    return {
-        type: constants.ADD_ARTICLE_REQUESTED
-    };
+  return {
+    type: constants.ADD_ARTICLE_REQUESTED
+  };
 }
 
 function AddArticleRejected(): AddArticleRejected {
-    return {
-        type: constants.ADD_ARTICLE_REJECTED
-    };
+  return {
+    type: constants.ADD_ARTICLE_REJECTED
+  };
 }
 
 function AddArticleFulfilled(articleLink: string): AddArticleFulfilled {
-    return {
-        type: constants.ADD_ARTICLE_FULFILLED,
-        articleLink: articleLink,
-        articleHash: SHA1.hex(articleLink)
-    };
+  return {
+    type: constants.ADD_ARTICLE_FULFILLED,
+    articleLink: articleLink,
+    articleHash: SHA1.hex(articleLink)
+  };
 }
 
 export function addArticle(articleLink: string) {
-    const user = auth().currentUser.uid;
+  const user = auth().currentUser.uid;
 
-    return (dispatch: Dispatch<any>) => {
-        dispatch(AddArticleRequested());
+  return (dispatch: Dispatch<any>) => {
+    dispatch(AddArticleRequested());
 
-        // Check if valid URL    
-        const regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-        if (regexp.test(articleLink)) {
-            const hash = SHA1.hex(articleLink);
-            const articleRef = database.ref('/userData/' + user + '/' + 'articles/' + hash);
-            articleRef.once('value').then(function (snapshot: any) {
-                // Check if article in database
-                if (snapshot.exists()) {
-                    alert('exists');
-                } else {
-                    articleRef
-                        .set({
-                            link: articleLink,
-                            id: hash,
-                            dateAdded: now.toLocaleDateString(),
-                            lastViewed: null,
-                            completed: false
-                        })
-                        .then(() => {
-                            dispatch(AddArticleFulfilled(articleLink));
-                        })
-                        .catch((error: string) => {
-                            console.log(error);
-                            dispatch(AddArticleRejected());
-                        });
-                }
-            });
+    // Check if valid URL
+    const regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    if (regexp.test(articleLink)) {
+      const hash = SHA1.hex(articleLink);
+      const articleRef = database.ref(
+        '/userData/' + user + '/' + 'articles/' + hash
+      );
+      // detach listener so current action doesn't trigger server syncing
+      articleRef.off();
+      articleRef.once('value').then(function(snapshot: any) {
+        // Check if article in database
+        if (snapshot.exists()) {
+          alert('exists');
         } else {
-            dispatch(AddArticleRejected());
-            console.log('invalid hyperlink');
+          articleRef
+            .set({
+              link: articleLink,
+              id: hash,
+              dateAdded: now.toLocaleDateString(),
+              lastViewed: null,
+              completed: false
+            })
+            .then(() => {
+              dispatch(AddArticleFulfilled(articleLink));
+              dispatch(ListenToFirebase());
+            })
+            .catch((error: string) => {
+              console.log(error);
+              dispatch(AddArticleRejected());
+            });
         }
-    };
+      });
+    } else {
+      dispatch(AddArticleRejected());
+      console.log('invalid hyperlink');
+    }
+  };
 }
