43e1d4fdc2fe9c6897be940f5a6063e8ebb6f45f
Jay Howard <jaydh@uw.edu>
2017-08-02 14:53:39 -0700

    Added tracker for git commit history

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,24 +2,53 @@ import * as React from 'react';
 import AddArticle from '../containers/AddArticle';
 import VisibleArticleList from '../containers/VisibleArticleList';
 import Logout from './Logout';
+import 'whatwg-fetch';
 
-class App extends React.Component {
+interface State {
+  gitCommit: string;
+}
 
-    public render() {
+class App extends React.Component<{}, State> {
+  constructor() {
+    super();
+    this.state = { gitCommit: 'not available' };
+  }
 
-        return (
-            <div className="container" >
-                <div className="page-header">
-                    <h1>wispy</h1>
-                </div>
-                <div className="jumbotron">
-                    <AddArticle />
-                    <VisibleArticleList />
-                </div>
-                <Logout />
-            </div >
-        );
-    }
+  // Gets repository information
+  componentWillMount() {
+    let that = this;
+    fetch('https://api.github.com/repos/jaydh/wisper')
+      .then(function(response: any) {
+        return response.json();
+      })
+      .then(function(json: any) {
+        console.log(json);
+        that.setState({ gitCommit: json.updated_at });
+      })
+      .catch(function(ex: any) {
+        console.log('parsing failed', ex);
+      });
+  }
+
+  public render() {
+    const gitDate = new Date(this.state.gitCommit);
+    return (
+      <div className="container">
+        <div className="page-header">
+          <h1>wispy</h1>
+        </div>
+        <div className="jumbotron">
+          <AddArticle />
+          <VisibleArticleList />
+        </div>
+        <Logout />
+        <p>
+          Under active development <br />
+          Last commit: {gitDate.toLocaleString()}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
