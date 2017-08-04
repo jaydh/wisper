7bb9252c88c9efa413827cea837c2d6138dae94b
Jay Howard <jaydh@uw.edu>
2017-08-04 15:53:24 -0700

    Projects now stored in firebase object and simply push the ids

--- src/actions/addArticleToProject.ts
+++ src/actions/addArticleToProject.ts
@@ -3,59 +3,60 @@ import { auth, database } from '../firebase';
 import { Dispatch } from 'react-redux';
 
 export interface AddArticleToProjectRequested {
-    type: constants.ADD_ARTICLE_TO_PROJECT_REQUESTED;
+  type: constants.ADD_ARTICLE_TO_PROJECT_REQUESTED;
 }
 export interface AddArticleToProjectFulfilled {
-    type: constants.ADD_ARTICLE_TO_PROJECT_FULFILLED;
-    articleHash: string;
-    project: string;
+  type: constants.ADD_ARTICLE_TO_PROJECT_FULFILLED;
+  articleHash: string;
+  project: string;
 }
 export interface AddArticleToProjectRejected {
-    type: constants.ADD_ARTICLE_TO_PROJECT_REJECTED;
+  type: constants.ADD_ARTICLE_TO_PROJECT_REJECTED;
 }
 
 function AddArticleToProjectRequested(): AddArticleToProjectRequested {
-    return {
-        type: constants.ADD_ARTICLE_TO_PROJECT_REQUESTED
-    };
+  return {
+    type: constants.ADD_ARTICLE_TO_PROJECT_REQUESTED
+  };
 }
 
 function AddArticleToProjectRejected(): AddArticleToProjectRejected {
-    return {
-        type: constants.ADD_ARTICLE_TO_PROJECT_REJECTED
-    };
+  return {
+    type: constants.ADD_ARTICLE_TO_PROJECT_REJECTED
+  };
 }
 
-function AddArticleToProjectFulfilled(articleHash: string, project: string): AddArticleToProjectFulfilled {
-    return {
-        type: constants.ADD_ARTICLE_TO_PROJECT_FULFILLED,
-        articleHash: articleHash,
-        project: project
-    };
+function AddArticleToProjectFulfilled(
+  articleHash: string,
+  project: string
+): AddArticleToProjectFulfilled {
+  return {
+    type: constants.ADD_ARTICLE_TO_PROJECT_FULFILLED,
+    articleHash: articleHash,
+    project: project
+  };
 }
 
 export function addArticleToProject(articleHash: string, project: string) {
-    const user = auth().currentUser.uid;
+  const user = auth().currentUser.uid;
 
-    return (dispatch: Dispatch<any>) => {
-        dispatch(AddArticleToProjectRequested());
+  return (dispatch: Dispatch<any>) => {
+    dispatch(AddArticleToProjectRequested());
 
-        const articleRef = database.ref('/userData/' + user + '/' + 'articles/' + articleHash + '/projects');
-        const projectRef = articleRef.push();
+    const projectRef = database.ref(
+      '/userData/' + user + '/' + 'articles/' + articleHash + '/projects'
+    );
 
-        articleRef.once('value', function (snapshot: any) {
-            projectRef
-                .set({
-                    projectId: project
-                })
-                .then(() => {
-                    dispatch(AddArticleToProjectFulfilled(articleHash, project));
-                })
-                .catch((error: string) => {
-                    console.log(error);
-                    dispatch(AddArticleToProjectRejected());
-                });
-
+    projectRef.once('value', function(snapshot: any) {
+      projectRef
+        .push(project)
+        .then(() => {
+          dispatch(AddArticleToProjectFulfilled(articleHash, project));
+        })
+        .catch((error: string) => {
+          console.log(error);
+          dispatch(AddArticleToProjectRejected());
         });
-    };
+    });
+  };
 }
