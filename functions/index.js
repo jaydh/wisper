b45a62a1257d8ee435490963c81b2839e7a1e76b
Jay Howard <jaydh@uw.edu>
2017-08-13 01:51:16 -0700

    Metadata only added to firebase if article wasn't deleted before processing

--- functions/index.js
+++ functions/index.js
@@ -9,15 +9,15 @@ exports.getMetadata = functions.database
   .onCreate(event => {
     const article = event.data.val();
     return scrape(article, (err, meta) => {
-      truthyMetadata = {}
+      truthyMetadata = {};
       Object.keys(meta).forEach(function(key) {
         if (meta[key]) {
           truthyMetadata[key] = meta[key];
         }
       });
-          event.data.ref.parent
-            .child('metadata')
-            .update(truthyMetadata);
+      // check if article was deleted before metadata was being processed
+      if (functions.database.ref('/userData/{uId}/articles/{articleID}/link')) {
+        event.data.ref.parent.child('metadata').update(truthyMetadata);
+      }
     });
-
   });
