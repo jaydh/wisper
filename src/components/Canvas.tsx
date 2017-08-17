e6dc63077127b3cc7eb5a3cdad97b39b252fbae7
Jay Howard <jayhoward300@gmail.com>
2017-08-17 01:06:01 -0700

    ArticleLists now draggable. Parent binding for dragging bounds not working

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -4,6 +4,7 @@ import { Button, Jumbotron } from 'react-bootstrap';
 // import { StoreState } from '../constants/StoreState';
 import { OrderedMap } from 'immutable';
 import { ArticleList } from '../constants/StoreState';
+let Draggable = require('react-draggable');
 
 interface Props {
   ListenToFirebase: any;
@@ -11,7 +12,42 @@ interface Props {
   articleLists: OrderedMap<string, ArticleList>;
 }
 
-export default class Canvas extends React.Component<Props> {
+interface State {
+  activeDrags: number;
+  deltaPosition: { x: number; y: number };
+}
+
+export default class Canvas extends React.Component<Props, State> {
+  constructor() {
+    super();
+    this.state = {
+      activeDrags: 0,
+      deltaPosition: {
+        x: 0,
+        y: 0
+      }
+    };
+    this.onStart = this.onStart.bind(this);
+    this.onStop = this.onStop.bind(this);
+  }
+  onStart() {
+    this.setState({ activeDrags: this.state.activeDrags + 1 });
+  }
+
+  onStop() {
+    this.setState({ activeDrags: this.state.activeDrags - 1 });
+  }
+
+  handleDrag(e: any, ui: any) {
+    const { x, y } = this.state.deltaPosition;
+    this.setState({
+      deltaPosition: {
+        x: x + ui.deltaX,
+        y: y + ui.deltaY
+      }
+    });
+  }
+
   componentWillMount() {
     const { ListenToFirebase, AddArticleList } = this.props;
     ListenToFirebase();
@@ -20,13 +56,26 @@ export default class Canvas extends React.Component<Props> {
 
   render() {
     const { articleLists, AddArticleList } = this.props;
+    const dragHandlers = { onStart: this.onStart, onStop: this.onStop };
     return (
-      <div className="container">
+      <div>
         <Button onClick={() => AddArticleList()}>Add List</Button>
-        <Jumbotron>
+        <Jumbotron className="canvas">
           {articleLists.map((articleList: ArticleList) => {
             return (
-              <VisibleArticleList key={articleList.id} id={articleList.id} />
+              <Draggable
+                key={articleList.id}
+                bounds=".canvas"
+                handle="strong"
+                {...dragHandlers}
+              >
+                <div className="box no-cursor">
+                  <strong className="cursor">
+                    <Button>Drag here</Button>
+                  </strong>
+                  <VisibleArticleList id={articleList.id} />
+                </div>
+              </Draggable>
             );
           })}
         </Jumbotron>
