e370a9f1c0d25b8dc7fdcd0ac1146279888b040f
Jay Howard <jaydh@uw.edu>
2017-08-31 20:12:45 -0700

    Refactor

--- src/containers/ProjectSelector.tsx
+++ src/containers/ProjectSelector.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { setProjectFilter } from '../actions/projectFilter';
 import { connect } from 'react-redux';
 import { List } from 'immutable';
-import {  DropdownButton, MenuItem } from 'react-bootstrap';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
 import { ArticleList as ArticleListType } from '../constants/StoreState';
 
 interface Props {
@@ -15,33 +15,26 @@ interface Props {
 class ProjectSelector extends React.Component<Props> {
   render() {
     const { projects, currentProject, onClick } = this.props;
+    const options = ['All', ...projects.toJS(), 'None'];
     return (
-        <DropdownButton
-          bsStyle="default"
-          title={currentProject}
-          noCaret={true}
-          id="dropdown-no-caret"
-        >
-          <MenuItem eventKey="1" onClick={() => onClick('ALL')}>
-            All
-          </MenuItem>
-          <MenuItem divider={true} />
-          {projects.map(project => {
-            return (
-              <MenuItem
-                eventKey={String(project)}
-                key={String(project)}
-                onClick={() => onClick(String(project))}
-              >
-                {project}
-              </MenuItem>
-            );
-          })}
-          <MenuItem divider={true} />
-          <MenuItem eventKey="4" onClick={() => onClick('NONE')}>
-            None
-          </MenuItem>
-        </DropdownButton>
+      <DropdownButton
+        bsStyle="default"
+        title={currentProject}
+        noCaret={true}
+        id="dropdown-no-caret"
+      >
+        {options.map(project => {
+          return (
+            <MenuItem
+              eventKey={String(project)}
+              key={String(project)}
+              onClick={() => onClick(String(project))}
+            >
+              {project}
+            </MenuItem>
+          );
+        })}
+      </DropdownButton>
     );
   }
 }
