5ffde57c9448a6f819b2d155b8c176cfacee3e11
Jay Howard <jaydh@uw.edu>
2017-08-31 17:47:10 -0700

    Implemented project selector with bootstrap dropdown components with onClicks

--- src/containers/ProjectSelector.tsx
+++ src/containers/ProjectSelector.tsx
@@ -1,50 +1,66 @@
 import * as React from 'react';
-import ProjectsFilterLink from '../containers/ProjectsFilterLink';
+import { setProjectFilter } from '../actions/projectFilter';
 import { connect } from 'react-redux';
 import { List } from 'immutable';
-// import { StoreState } from '../constants/StoreState';
+import {  DropdownButton, MenuItem } from 'react-bootstrap';
+import { ArticleList as ArticleListType } from '../constants/StoreState';
 
 interface Props {
   projects: List<String>;
+  currentProject: string;
+  onClick: any;
   id: string;
 }
 
-class ProjectsFooter extends React.Component<Props> {
+class ProjectSelector extends React.Component<Props> {
   render() {
-    const { projects, id } = this.props;
+    const { projects, currentProject, onClick } = this.props;
     return (
-      <div>
-        <p>
-          Project: {' '}
-          <ProjectsFilterLink filter="ALL" id={id}>
+        <DropdownButton
+          bsStyle="default"
+          title={currentProject}
+          noCaret={true}
+          id="dropdown-no-caret"
+        >
+          <MenuItem eventKey="1" onClick={() => onClick('ALL')}>
             All
-          </ProjectsFilterLink>
-          {projects
-            ? projects.map(project => {
-                return (
-                  <span key={String(project)}>
-                    {', '}
-                    <ProjectsFilterLink filter={project} id={id}>
-                      {project}
-                    </ProjectsFilterLink>
-                  </span>
-                );
-              })
-            : <p>No project Filters</p>}
-          {', '}
-          <ProjectsFilterLink filter="NONE" id={id}>
+          </MenuItem>
+          <MenuItem divider={true} />
+          {projects.map(project => {
+            return (
+              <MenuItem
+                eventKey={String(project)}
+                key={String(project)}
+                onClick={() => onClick(String(project))}
+              >
+                {project}
+              </MenuItem>
+            );
+          })}
+          <MenuItem divider={true} />
+          <MenuItem eventKey="4" onClick={() => onClick('NONE')}>
             None
-          </ProjectsFilterLink>
-        </p>
-      </div>
+          </MenuItem>
+        </DropdownButton>
     );
   }
 }
 
-const mapStateToProps = (state: any, ownProps: any) => {
+const mapDispatchToProps = (dispatch: any, ownProps: Props) => {
   return {
-    projects: state.get('projects')
+    onClick: (filter: string) => {
+      dispatch(setProjectFilter(filter, ownProps.id));
+    }
   };
 };
 
-export default connect(mapStateToProps)(ProjectsFooter);
+const mapStateToProps = (state: any, ownProps: any) => {
+  return {
+    projects: state.get('projects'),
+    currentProject: state
+      .get('articleLists')
+      .find((list: ArticleListType) => list.id === ownProps.id).projectFilter
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectSelector);
