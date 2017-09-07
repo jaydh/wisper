d3ea2831d246e2e11db3d7e4632df5cef82a7c1c
Jay Howard <jaydh@uw.edu>
2017-09-06 23:30:47 -0700

    Lint

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,14 +1,13 @@
-import { Button,ButtonGroup, Jumbotron } from 'react-bootstrap';
+import { Button, ButtonGroup, Jumbotron } from 'react-bootstrap';
 const { bootstrapUtils } = require('react-bootstrap/lib/utils');
 
 export default () => {
   bootstrapUtils.addStyle(Button, 'more');
   bootstrapUtils.addStyle(Button, 'addList');
-  bootstrapUtils.addStyle(Button, 'analytics');  
+  bootstrapUtils.addStyle(Button, 'analytics');
   bootstrapUtils.addStyle(Button, 'drag');
   bootstrapUtils.addStyle(Button, 'submit');
-  bootstrapUtils.addStyle(Button, 'logout');  
+  bootstrapUtils.addStyle(Button, 'logout');
   bootstrapUtils.addStyle(Jumbotron, 'canvas');
   bootstrapUtils.addStyle(ButtonGroup, 'article');
-  
 };
