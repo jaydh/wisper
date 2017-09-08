dc937872b8fd8415324f07c4555b5f01f6a2677b
Jay Howard <jaydh@uw.edu>
2017-09-08 06:04:27 -0700

    Bug fixes for saving pos metadata on firebase

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 const scrape = require('scrape-metadata');
 const fetch = require('node-fetch');
 const pos = require('pos');
-const { Set, fromJS } = require('immutable');
+const { Set, List, fromJS } = require('immutable');
 admin.initializeApp(functions.config().firebase);
 
 exports.getMetadata = functions.database
@@ -26,11 +26,12 @@ exports.getMetadata = functions.database
     });
   });
 
-exports.addKeywordsFromMetadata = functions.database.ref(
-  '/userData/{uID}/articles/{articleID}/projects/{projectID}'.onCreate(
-    event => {
+exports.addKeywordsFromMetadata = functions.database
+  .ref('/userData/{uID}/articles/{articleID}/projects/{projectID}')
+  .onCreate(event => {
+    const userDataRef = event.data.ref.parent.parent.parent.parent;
+    return userDataRef.once('value').then(function(snapshot) {
       const project = event.data.val();
-      const article = event.parent.parent.val();
       const metaToKeep = fromJS([
         'ogTitle',
         'title',
@@ -40,38 +41,58 @@ exports.addKeywordsFromMetadata = functions.database.ref(
       ]);
       const tagsToKeep = fromJS(['JJ', 'NN', 'NNP', 'NNPS', 'VB']);
       const tagger = new pos.Tagger();
-      console.log('test', project, article);
+      const userData = snapshot.val();
+      const article = userData.articles[event.params.articleID];
+      const [projectKey, projectValue] = fromJS(userData['projects']).findEntry(
+        t => t.get('id') === project
+      );
+      const dictionary = projectValue.get('dictionary');
+      if (article.metadata) {
+        let newWords = Set(dictionary);
+        const meta = fromJS(article.metadata);
+        meta
+          .keySeq()
+          .filter(t => metaToKeep.includes(t))
+          .forEach(t => {
+            const words = new pos.Lexer().lex(meta.get(t));
+            const taggedWords = fromJS(tagger.tag(words))
+              .filter(p => {
+                return tagsToKeep.includes(p.get(1));
+              })
+              .map(p => p.get(0));
+            newWords = newWords.union(taggedWords.valueSeq());
+          });
+        userDataRef
+          .child('projects')
+          .child(projectKey)
+          .update({ dictionary: newWords.toJS() });
+      }
+    });
+  });
 
-      firebase
-        .database()
-        .ref(
-          '/userData/' +
-            event.params.uID +
-            '/projects/' +
-            event.params.projectID
-        )
-        .once('value')
-        .then(function(snapshot) {
-          const dictionary = snapshot.val().dictionary;
-          console.log('dictionary', dictionary);
-          if (article.meta) {
-            let newWords = fromJS(dictionary).valueSeq;
-            const meta = fromJS(article.metadata);
-            meta
-              .keySeq()
-              .filter(t => metaToKeep.includes(t))
-              .forEach(t => {
-                const words = new pos.Lexer().lex(meta.get(t));
-                const taggedWords = fromJS(tagger.tag(words))
-                  .filter(p => {
-                    return tagsToKeep.includes(p.get(1));
-                  })
-                  .map(p => p.get(0));
-                newWords = newWords.union(taggedWords.valueSeq());
-              });
-            console.log(newWords);
-          }
+/*
+exports.getSynonyms = functions.database
+  .ref('/userData/{uID}/projects/{projectID}')
+  .onCreate(event => {
+    fetch(
+      'http://words.bighugelabs.com/api/2/b0ccfcccd889eeb6a11c013493465013/' +
+        event.params.projectID +
+        '/json'
+    )
+      .then(function() {
+        return response.json();
+      })
+      .then(function() {
+        let dictionary = fromJS([]);
+        const synonymGroups = fromJS(json).toList();
+        synonymGroups.forEach(t => {
+          dictionary = dictionary.merge(t.get('syn'));
         });
-    }
-  )
-);
+        return dictionary;
+      })
+      .then(function(dictionary) {
+        event.data.ref.set({
+          dictionary: dictionary.valueSeq().toJS()
+        });
+      });
+  });*/
