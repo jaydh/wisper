d5f8e025dfd0efe0ec6f1f6bd9a3ea17dd254b60
Jay Howard <jaydh@uw.edu>
2017-09-30 00:11:50 +0000

    Added feature to hide dailies when they're all completed for the day

--- src/components/Dailies.tsx
+++ src/components/Dailies.tsx
@@ -10,7 +10,7 @@ import { connect } from 'react-redux';
 import addDaily from '../actions/dailies/addDaily';
 import completeDaily from '../actions/dailies/completeDaily';
 import { Daily } from '../constants/StoreState';
-import { List } from 'immutable';
+import { List, OrderedSet } from 'immutable';
 
 interface Props {
   onComplete: (id: string) => void;
@@ -55,22 +55,30 @@ class Dailies extends React.Component<Props, State> {
           </FormGroup>
         </Form>
 
-        {!dailies.isEmpty() && <Jumbotron>
-          {dailies
-            .filter((t: Daily) => {
-              return t.completedOn && !t.completedOn.isEmpty()
-                ? t.completedOn.last().toDateString() !==
-                new Date().toDateString()
-                : true;
-            })
-            .map((t: Daily) => {
-              return (
-                <Button key={t.id} onClick={() => onComplete(t.id)}>
-                  {t.title}
-                </Button>
-              );
-            })}
-        </Jumbotron>}
+        { // Only show if there are dailies active
+          dailies
+          .map((t: Daily) => t.completedOn)
+          .filter(
+            (t: OrderedSet<Date>) =>
+              t ? t.last().toDateString() !== new Date().toDateString() : true
+          ).size !== 0 && (
+          <Jumbotron>
+            {dailies
+              .filter((t: Daily) => {
+                return t.completedOn && !t.completedOn.isEmpty()
+                  ? t.completedOn.last().toDateString() !==
+                    new Date().toDateString()
+                  : true;
+              })
+              .map((t: Daily) => {
+                return (
+                  <Button key={t.id} onClick={() => onComplete(t.id)}>
+                    {t.title}
+                  </Button>
+                );
+              })}
+          </Jumbotron>
+        )}
       </div>
     );
   }
