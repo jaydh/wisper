d533bf4baeaa701c99127512777497f73684e14d
Jay Howard <jaydh@uw.edu>
2017-10-06 01:20:11 -0700

    Added a page refresh when login component is unmounted so purged local storage is released

--- src/components/LoginLoading.tsx
+++ src/components/LoginLoading.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import { auth, provider } from '../firebase';
 import { Button } from 'react-bootstrap';
 export default class LoginLoading extends React.Component {
+  componentWillUnmount() {
+    window.location.reload();
+  }
   render() {
     const wellStyles = { maxWidth: 400, margin: 'auto' };
-
     return (
       <div className="well" style={wellStyles}>
         <h2> Create account</h2>
