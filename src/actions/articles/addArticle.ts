ff18f124edb527ac019c1af576921fcdb2b0b646
Jay Howard <jaydh@uw.edu>
2017-10-26 17:55:08 -0700

    Chained demo dispatched actions into promises

--- src/actions/articles/addArticle.ts
+++ src/actions/articles/addArticle.ts
@@ -65,68 +65,72 @@ export default function addArticle(articleLink: string, project?: string) {
     let projectWithKey = {};
     projectWithKey[projectPushKey] = project ? project : null;
 
-    articleRef.once('value').then(function(snapshot: any) {
-      // Check if article in database
-      if (snapshot.exists()) {
-        dispatch(AddArticleRejected());
-      } else {
-        articleRef
-          .set({
-            link: articleLink,
-            id: hash,
-            dateAdded: now.toLocaleString(),
-            completed: false,
-            fetching: true
-          })
-          .then(articleRef.child('projects').update(projectWithKey))
-          .then(() => {
-            dispatch(AddArticleFulfilled(articleLink, project));
-          })
-          .catch((error: string) => {
-            console.log(error);
-            dispatch(AddArticleRejected());
-          });
-      }
-    });
-    if (project) {
-      const projects = database.ref('/userData/' + user + '/projects/');
+    return articleRef
+      .once('value')
+      .then(function(snapshot: any) {
+        // Check if article in database
+        if (snapshot.exists()) {
+          dispatch(AddArticleRejected());
+        } else {
+          articleRef
+            .set({
+              link: articleLink,
+              id: hash,
+              dateAdded: now.toLocaleString(),
+              completed: false,
+              fetching: true
+            })
+            .then(articleRef.child('projects').update(projectWithKey))
+            .then(() => {
+              dispatch(AddArticleFulfilled(articleLink, project));
+            })
+            .catch((error: string) => {
+              console.log(error);
+              dispatch(AddArticleRejected());
+            });
+        }
+      })
+      .then(() => {
+        if (project) {
+          const projects = database.ref('/userData/' + user + '/projects/');
 
-      // Gets dictionary data for project and updates project dicionary accordingly
-      fetch(
-        'https://words.bighugelabs.com/api/2/b0ccfcccd889eeb6a11c013493465013/' +
-          project +
-          '/json'
-      )
-        .then(function(response: any) {
-          return response.json();
-        })
-        .then(function(json: any) {
-          let dictionary: List<string> = fromJS([]);
-          const synonymGroups = fromJS(json).toList();
-          synonymGroups.forEach((t: any) => {
-            dictionary = dictionary.merge(t.get('syn'));
-          });
-          return dictionary;
-        })
-        .then(function(dictionary: any) {
-          projects.once('value').then(function(snapshot: any) {
-            const push = () =>
-              projects.push({
-                id: project,
-                dictionary: dictionary.valueSeq().toJS()
+          // Gets dictionary data for project and updates project dicionary accordingly
+          fetch(
+            'https://words.bighugelabs.com/api/2/b0ccfcccd889eeb6a11c013493465013/' +
+              project +
+              '/json'
+          )
+            .then(function(response: any) {
+              return response.json();
+            })
+            .then(function(json: any) {
+              let dictionary: List<string> = fromJS([]);
+              const synonymGroups = fromJS(json).toList();
+              synonymGroups.forEach((t: any) => {
+                dictionary = dictionary.merge(t.get('syn'));
               });
-            if (snapshot.val()) {
-              const proj = fromJS(snapshot.val())
-                .valueSeq()
-                .map((t: Map<string, any>) => t.get('id'));
-              if (!proj.includes(project)) {
-                push();
-              }
-            } else {
-              push();
-            }
-          });
-        });
-    }
+              return dictionary;
+            })
+            .then(function(dictionary: any) {
+              projects.once('value').then(function(snapshot: any) {
+                const push = () =>
+                  projects.push({
+                    id: project,
+                    dictionary: dictionary.valueSeq().toJS()
+                  });
+                if (snapshot.val()) {
+                  const proj = fromJS(snapshot.val())
+                    .valueSeq()
+                    .map((t: Map<string, any>) => t.get('id'));
+                  if (!proj.includes(project)) {
+                    push();
+                  }
+                } else {
+                  push();
+                }
+              });
+            });
+        }
+      });
   };
 }
--- src/constants/demo.ts
+++ src/constants/demo.ts
@@ -17,111 +17,130 @@ import completeDaily from '../actions/dailies/completeDaily';
 export default function(store: any, persistor: any) {
   persistor.purge();
 
-  store.dispatch(
-    addArticle(
-      'https://lbpost.com/life/pets/2000011459-pet-store-bill-passes-california-senate-38-to-0',
-      'news'
+  store
+    .dispatch(
+      addArticle(
+        'https://lbpost.com/life/pets/2000011459-pet-store-bill-passes-california-senate-38-to-0',
+        'news'
+      )
     )
-  );
-  store.dispatch(
-    addArticle(
-      'https://www.independent.co.uk/news/uk/home-news/billy-caldwell-cannabis-oil-boy-seizures-stopped-cured-prescription-medical-marijuana-a7933066.html',
-      'news'
+    .then(() =>
+      store.dispatch(
+        addArticle(
+          'https://www.independent.co.uk/news/uk/home-news/billy-caldwell-cannabis-oil-boy-seizures-stopped-cured-prescription-medical-marijuana-a7933066.html',
+          'news'
+        )
+      )
     )
-  );
-  store.dispatch(
-    addArticle(
-      'https://www.theguardian.com/us-news/2017/aug/16/kkk-permit-denied-cross-burning-stone-mountain-georgia',
-      'news'
+    .then(() =>
+      store.dispatch(
+        addArticle(
+          'https://www.theguardian.com/us-news/2017/aug/16/kkk-permit-denied-cross-burning-stone-mountain-georgia',
+          'news'
+        )
+      )
     )
-  );
-  store.dispatch(
-    addArticle(
-      'https://www.bloomberg.com/news/articles/2017-08-28/amazon-cuts-prices-at-whole-foods-as-much-as-50-on-first-day',
-      'news'
+    .then(() =>
+      store.dispatch(
+        addArticle(
+          'https://www.bloomberg.com/news/articles/2017-08-28/amazon-cuts-prices-at-whole-foods-as-much-as-50-on-first-day',
+          'news'
+        )
+      )
     )
-  );
-
-  store.dispatch(
-    addArticle(
-      'http://abcnews.go.com/US/wireStory/woman-chose-baby-chemotherapy-died-49723010',
-      'news'
+    .then(() =>
+      store.dispatch(
+        addArticle(
+          'http://abcnews.go.com/US/wireStory/woman-chose-baby-chemotherapy-died-49723010',
+          'news'
+        )
+      )
     )
-  );
-
-  store.dispatch(
-    addArticle(
-      'https://www.theguardian.com/society/2017/aug/24/woman-jailed-10-years-false-rape-claims-jemma-beale',
-      'news'
+    .then(() =>
+      store.dispatch(
+        addArticle(
+          'https://www.theguardian.com/society/2017/aug/24/woman-jailed-10-years-false-rape-claims-jemma-beale',
+          'news'
+        )
+      )
     )
-  );
-
-  store.dispatch(
-    addArticle(
-      'https://www.washingtonpost.com/news/morning-mix/wp/2017/09/08/after-utah-nurses-violent-arrest-local-prosecutors-ask-fbi-to-help-investigate-police/?utm_term=.a7de4786e701',
-      'news'
+    .then(() =>
+      store.dispatch(
+        addArticle(
+          'https://www.washingtonpost.com/news/morning-mix/wp/2017/09/08/after-utah-nurses-violent-arrest-local-prosecutors-ask-fbi-to-help-investigate-police/?utm_term=.a7de4786e701',
+          'news'
+        )
+      )
     )
-  );
-
-  store.dispatch(
-    addArticle(
-      'https://www.washingtonpost.com/news/post-nation/wp/2017/09/05/teen-girl-files-claim-against-police-who-mistook-her-for-a-black-male-suspect-and-punched-her/?utm_term=.6497346f0a84',
-      'news'
+    .then(() =>
+      store.dispatch(
+        addArticle(
+          'https://www.washingtonpost.com/news/post-nation/wp/2017/09/05/teen-girl-files-claim-against-police-who-mistook-her-for-a-black-male-suspect-and-punched-her/?utm_term=.6497346f0a84',
+          'news'
+        )
+      )
     )
-  );
-
-  store.dispatch(
-    addArticle(
-      'https://www.washingtonpost.com/news/business/wp/2017/08/22/not-one-drop-of-poland-spring-bottled-water-is-from-a-spring-lawsuit-claims/',
-      'news'
+    .then(() =>
+      store.dispatch(
+        addArticle(
+          'https://www.washingtonpost.com/news/business/wp/2017/08/22/not-one-drop-of-poland-spring-bottled-water-is-from-a-spring-lawsuit-claims/',
+          'news'
+        )
+      )
     )
-  );
-
-  store.dispatch(
-    addArticle(
-      'https://www.bloomberg.com/news/articles/2017-09-05/fema-is-almost-out-of-money-as-hurricane-irma-threatens-florida',
-      'news'
+    .then(() =>
+      store.dispatch(
+        addArticle(
+          'https://www.bloomberg.com/news/articles/2017-09-05/fema-is-almost-out-of-money-as-hurricane-irma-threatens-florida',
+          'news'
+        )
+      )
     )
-  );
-
-  store.dispatch(
-    addArticle(
-      'http://www.msn.com/en-us/news/us/mattis-orders-pentagon-to-allow-transgender-troops-to-continue-serving-pending-study/ar-AAqX5NJ?ocid=spartanntp',
-      'news'
+    .then(() =>
+      store.dispatch(
+        addArticle(
+          'http://www.msn.com/en-us/news/us/mattis-orders-pentagon-to-allow-transgender-troops-to-continue-serving-pending-study/ar-AAqX5NJ?ocid=spartanntp',
+          'news'
+        )
+      )
     )
-  );
-
-  store.dispatch(
-    addArticle(
-      'https://gamereactor.eu/news/585493/Microsoft+confirm+theyre+in+talks+with+Sony+about+crossplay/',
-      'games'
+    .then(() =>
+      store.dispatch(
+        addArticle(
+          'https://gamereactor.eu/news/585493/Microsoft+confirm+theyre+in+talks+with+Sony+about+crossplay/',
+          'games'
+        )
+      )
     )
-  );
-
-  store.dispatch(
-    addArticle(
-      'http://www.dsogaming.com/news/despite-square-enixs-promises-and-after-almost-six-months-nier-automata-has-not-received-any-patch-on-the-pc/',
-      'games'
+    .then(() =>
+      store.dispatch(
+        addArticle(
+          'http://www.dsogaming.com/news/despite-square-enixs-promises-and-after-almost-six-months-nier-automata-has-not-received-any-patch-on-the-pc/',
+          'games'
+        )
+      )
     )
-  );
-
-  store.dispatch(
-    addArticle(
-      'https://www.pcgamesn.com/half-life/dota-2-killed-half-life-3',
-      'games'
+    .then(() =>
+      store.dispatch(
+        addArticle(
+          'https://www.pcgamesn.com/half-life/dota-2-killed-half-life-3',
+          'games'
+        )
+      )
     )
-  );
-
-  store.dispatch(
-    addArticle(
-      'https://www.polygon.com/2017/8/21/16177270/htc-vive-price-cut-599?utm_campaign=polygon&utm_content=chorus&utm_medium=social&utm_source=twitter',
-      'games'
+    .then(() =>
+      store.dispatch(
+        addArticle(
+          'https://www.polygon.com/2017/8/21/16177270/htc-vive-price-cut-599?utm_campaign=polygon&utm_content=chorus&utm_medium=social&utm_source=twitter',
+          'games'
+        )
+      )
     )
-  );
-
-  store.dispatch(
-    addArticle('https://www.youtube.com/watch?v=NWagBjDMwTU', 'games')
-  );
+    .then(() =>
+      store.dispatch(
+        addArticle('https://www.youtube.com/watch?v=NWagBjDMwTU', 'games')
+      )
+    );
 
   store.dispatch(addArticleList('0'));
   store.dispatch(addArticleList('1'));
