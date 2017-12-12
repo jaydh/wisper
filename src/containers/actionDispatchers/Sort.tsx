f5725f1da79c92152074f40cc47901147a1b528f
Jay Howard <jaydh@uw.edu>
2017-12-12 01:54:44 -0800

    Added glyphicons for sort menu and added current sort glpyhicon

--- src/containers/actionDispatchers/Sort.tsx
+++ src/containers/actionDispatchers/Sort.tsx
@@ -1,41 +1,88 @@
 import * as React from 'react';
-import { Dropdown, MenuItem } from 'react-bootstrap';
+import { Dropdown, MenuItem, Glyphicon } from 'react-bootstrap';
 import setSortFilter from '../../actions/setSortFilter';
 import { connect } from 'react-redux';
+import { ArticleList } from '../../constants/StoreState';
 
 interface Props {
   onClick: (t: string) => void;
+  currentSort: string;
 }
 
 class Sort extends React.Component<Props> {
   render() {
-    const { onClick } = this.props;
+    const { onClick, currentSort } = this.props;
+    const glyph = (function(sort: string) {
+      switch (sort) {
+        case 'date-asc':
+          return 'sort-by-order';
+        case 'date-desc':
+          return 'sort-by-order-alt';
+        case 'title':
+          return 'sort-by-alphabet';
+        case 'title-reverse':
+          return 'sort-by-alphabet-alt';
+        case 'dateRead':
+          return 'sort-by-attributes';
+        case 'dateRead-reverse':
+          return 'sort-by-attributes-alt';
+        default:
+          return '';
+      }
+    })(currentSort);
     return (
       <Dropdown id="bg-nested-dropdown">
-        <Dropdown.Toggle noCaret={true}>Sort</Dropdown.Toggle>
+        <Dropdown.Toggle>
+          Sort <Glyphicon glyph={glyph} />
+        </Dropdown.Toggle>
         <Dropdown.Menu className="filter-dropdown ">
           <MenuItem
-            eventKey="1"
-            onClick={() => {
-              onClick('date-asc');
-            }}
+            onClick={() =>
+              currentSort === 'title'
+                ? onClick('title-reverse')
+                : onClick('title')
+            }
           >
-            by date added (recent first)
+            <Glyphicon
+              glyph={
+                currentSort === 'title'
+                  ? 'sort-by-alphabet-alt'
+                  : 'sort-by-alphabet'
+              }
+            />{' '}
+            Title
           </MenuItem>
           <MenuItem
-            eventKey="2"
             onClick={() => {
-              onClick('date-desc');
+              currentSort === 'date-asc'
+                ? onClick('date-desc')
+                : onClick('date-asc');
             }}
           >
-            by date added (recent last)
+            <Glyphicon
+              glyph={
+                currentSort === 'date-asc'
+                  ? 'sort-by-order-alt'
+                  : 'sort-by-order'
+              }
+            />{' '}
+            Added
           </MenuItem>
-
-          <MenuItem eventKey="3" onClick={() => onClick('title')}>
-            by title
-          </MenuItem>
-          <MenuItem eventKey="4" onClick={() => onClick('dateRead')}>
-            by date read
+          <MenuItem
+            onClick={() =>
+              currentSort === 'dateRead'
+                ? onClick('dateRead-reverse')
+                : onClick('dateRead')
+            }
+          >
+            <Glyphicon
+              glyph={
+                currentSort === 'dateRead'
+                  ? 'sort-by-attributes-alt'
+                  : 'sort-by-attributes'
+              }
+            />{' '}
+            Read
           </MenuItem>
         </Dropdown.Menu>
       </Dropdown>
@@ -51,4 +98,12 @@ function mapDispatchToProps(dispatch: any, ownProps: any) {
   };
 }
 
-export default connect(null, mapDispatchToProps)(Sort);
+function mapStateToProps(state: any, ownProps: any) {
+  return {
+    currentSort: state
+      .get('articleLists')
+      .find((t: ArticleList) => t.id === ownProps.id).sort
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sort);
