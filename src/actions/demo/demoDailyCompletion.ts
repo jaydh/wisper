5fbe0fcccd7d04fc7c1394c995a4bc4e4464824c
Jay Howard <jaydh@uw.edu>
2017-12-22 04:24:14 -0800

    Change demo daily completions to 150 iterations

--- src/actions/demo/demoDailyCompletion.ts
+++ src/actions/demo/demoDailyCompletion.ts
@@ -62,7 +62,7 @@ export default function demoDailyCompletion(ids: List<string>) {
           );
 
           let completedOn: List<Date> = List();
-          for (let j = 1; j < 100; j++) {
+          for (let j = 1; j < 150; j++) {
             completedOn = completedOn.push(subDays(new Date(), j));
             Math.floor(Math.random() < 0.1 * (key + 1) ? j++ : (j = j));
           }
