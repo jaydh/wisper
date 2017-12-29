d4b3f491835f3c3be0b5e00bcdbd5d12dec6039d
Jay Howard <jaydh@uw.edu>
2017-12-28 17:11:22 -0800

    Styling

--- src/components/LoginLoading.tsx
+++ src/components/LoginLoading.tsx
@@ -1,38 +1,40 @@
 import * as React from 'react';
 import { auth, provider } from '../firebase';
+import GitInfo from './GitInfo';
 import { Well, Button } from 'react-bootstrap';
 export default class LoginLoading extends React.Component {
   render() {
     return (
-      <div className="container-fluid login">
-        <Well style={{ maxWidth: '40rem', margin: 'auto' }}>
-          <Button
-            bsStyle="daily"
-            bsSize="large"
-            onClick={() => auth().signInWithRedirect(provider)}
-            block={true}
-          >
-            Authenticate with Google{' '}
-          </Button>
-          <Button
-            bsStyle="daily"
-            bsSize="large"
-            onClick={() => auth().signInAnonymously()}
-            block={true}
-          >
-            Demo{' '}
-          </Button>
+      <Well style={{ maxWidth: '40rem', margin: 'auto' }}>
+        <Button
+          bsStyle="daily"
+          bsSize="large"
+          onClick={() => auth().signInWithRedirect(provider)}
+          block={true}
+        >
+          Continue with Google Authentication{' '}
+        </Button>
+        <Button
+          bsStyle="daily"
+          bsSize="large"
+          onClick={() => auth().signInAnonymously()}
+          block={true}
+        >
+          Demo{' '}
+        </Button>
+        <br />
+        <p>
+          Wispy uses Google authentication to identify users without requesting
+          user data. We redirect you to authenticate with google, generate a
+          user id, and recieve the authentication response from Google.
           <br />
-          <p>
-            Wispy uses Google authentication to identify users and doesn't
-            request any user data from Google
-            <br />
-            The demo is a demonstration of Wispy that dynamically simulates user
-            data. Actions are triggered in real time and the data is persistent
-            and fully tied to each demo account.
-          </p>
-        </Well>
-      </div>
+          The demo is a demonstration of Wispy that dynamically simulates user
+          data. Actions are triggered in real time and the data is persistent
+          and fully tied to each demo account.
+          <br />
+        </p>
+        <GitInfo />
+      </Well>
     );
   }
 }
