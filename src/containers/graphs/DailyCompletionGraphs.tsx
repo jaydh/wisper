1687d57a3c3075eb1cc2112c0ca4f54a32fa379f
Jay Howard <jaydh@uw.edu>
2017-12-28 16:10:41 -0800

    Make DailyCompletiongraphs respond to min/max

--- src/containers/graphs/DailyCompletionGraphs.tsx
+++ src/containers/graphs/DailyCompletionGraphs.tsx
@@ -4,67 +4,75 @@ import { Doughnut } from 'react-chartjs-2';
 import { List } from 'immutable';
 import { Daily } from '../../constants/StoreState';
 import {
-  endOfDay,
   isAfter,
-  subWeeks,
-  differenceInCalendarDays
+  differenceInCalendarDays,
+  isBefore,
+  isSameDay
 } from 'date-fns';
 import { Row, Col } from 'react-bootstrap';
 
 interface Props {
   dailies: List<Daily>;
+  graphMin: Date;
+  graphMax: Date;
+  absMin: Date;
 }
 
 class DailyGraph extends React.Component<Props> {
- 
-  getCompletionData(recent: boolean) {
-    return this.props.dailies.map((t: Daily, key: number) => {
-      const cutOff = endOfDay(subWeeks(new Date(), 4));
-      const completionPercentage =
-        recent && !isAfter(t.completedOn.first(), cutOff)
-          ? t.completedOn.filter((p: Date) => isAfter(p, cutOff)).size /
-            28 *
-            100
-          : t.completedOn.size /
-            (differenceInCalendarDays(new Date(), t.completedOn.first()) + 1) *
-            100;
-      const data = {
-        datasets: [
-          {
-            label: t.title + recent ? ' recent' : ' overall',
-            data: [completionPercentage, 100 - completionPercentage],
-            backgroundColor: ['#60a5f3', '#FFFFFF']
-          }
-        ],
-        labels: ['Completion Percentage', 'Incompletion Percentage']
-      };
-      const options = {
-        title: {
-          display: true,
-          text: t.title
-        },
-        legend: {
-          display: false
-        },
-        tooltips: {
-          callbacks: {
-            label: function(tooltipItem: any, callbackData: any) {
-              return (
-                callbackData.datasets[0].data[tooltipItem.index].toFixed(2) +
-                '%'
-              );
+  getCompletionData() {
+    return this.props.dailies
+      .filter((t: Daily) => !t.finalized)
+      .map((t: Daily, key: number) => {
+        const graphMax = this.props.graphMax ? this.props.graphMax : new Date();
+        const graphMin = this.props.graphMin
+          ? this.props.graphMin
+          : this.props.absMin;
+        const filterData = t.completedOn.filter(
+          (p: Date) =>
+            (isBefore(p, graphMax) && isAfter(p, graphMin)) ||
+            isSameDay(p, graphMax) ||
+            isSameDay(p, graphMin)
+        );
+        const completionPercentage =
+          filterData.size /
+          (differenceInCalendarDays(graphMax, graphMin) + 1) *
+          100;
+        const data = {
+          datasets: [
+            {
+              label: t.title,
+              data: [completionPercentage, 100 - completionPercentage],
+              backgroundColor: ['#60a5f3', '#FFFFFF']
+            }
+          ],
+          labels: ['Completion Percentage', 'Incompletion Percentage']
+        };
+        const options = {
+          title: {
+            display: true,
+            text: t.title
+          },
+          legend: {
+            display: false
+          },
+          tooltips: {
+            callbacks: {
+              label: function(tooltipItem: any, callbackData: any) {
+                return (
+                  callbackData.datasets[0].data[tooltipItem.index].toFixed(2) +
+                  '%'
+                );
+              }
             }
           }
-        }
-      };
-      return [data, options];
-    });
+        };
+        return [data, options];
+      });
   }
 
   render() {
     const { dailies } = this.props;
-    const recentCompletion = this.getCompletionData(true) as any;
-    const overallCompletion = this.getCompletionData(false) as any;
+    const completionData = this.getCompletionData() as any;
 
     return (
       <div>
@@ -78,21 +86,7 @@ class DailyGraph extends React.Component<Props> {
               </Col>
             </Row>
             <Row>
-              {recentCompletion.map((value: any, key: string) => (
-                <Col key={key} xs={6} sm={4} md={3} lg={2}>
-                  <Doughnut key={key} data={value[0]} options={value[1]} />
-                </Col>
-              ))}
-            </Row>
-            <Row>
-              <Col>
-                <h2 style={{ textAlign: 'center' }}>
-                  Overall Completion Percentage
-                </h2>
-              </Col>
-            </Row>
-            <Row>
-              {overallCompletion.map((value: any, key: string) => (
+              {completionData.map((value: any, key: string) => (
                 <Col key={key} xs={6} sm={4} md={3} lg={2}>
                   <Doughnut key={key} data={value[0]} options={value[1]} />
                 </Col>
@@ -106,7 +100,13 @@ class DailyGraph extends React.Component<Props> {
 }
 const mapStateToProps = (state: any, ownProps: any) => {
   return {
-    dailies: state.get('dailies')
+    dailies: state.get('dailies'),
+    graphMin: state.get('ui').dailyGraphMin,
+    graphMax: state.get('ui').dailyGraphMax,
+    absMin: state
+      .get('dailies')
+      .map((t: Daily) => t.completedOn.first())
+      .min()
   };
 };
 
