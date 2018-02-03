bc8e4f759465bdf89755693f1e331fa96edc6d92
Jay Howard <jaydh@uw.edu>
2018-02-03 10:18:30 -0800

    Add styling to articleView

--- src/containers/ArticleView.tsx
+++ src/containers/ArticleView.tsx
@@ -2,7 +2,17 @@ import * as React from 'react';
 
 import { connect } from 'react-redux';
 import { Article as ArticleType } from '../constants/StoreState';
-import { Jumbotron, Nav, Navbar, NavbarBrand, NavItem } from 'reactstrap';
+import {
+  Collapse,
+  Fade,
+  Jumbotron,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavItem,
+  Button
+} from 'reactstrap';
+import { Icon } from 'react-fa';
 import ArticleMenu from './ArticleMenu';
 import ReactHTMLParser from 'react-html-parser';
 import ExitArticleView from '../containers/actionDispatchers/ExitArticleView';
@@ -13,6 +23,8 @@ interface Props {
 interface State {
   scrollPosition: number;
   scrollUp: boolean;
+  ticking: boolean;
+  showDetails: boolean;
 }
 
 class ArticleView extends React.Component<Props, State> {
@@ -20,7 +32,9 @@ class ArticleView extends React.Component<Props, State> {
     super(props);
     this.state = {
       scrollPosition: window.scrollY,
-      scrollUp: true
+      scrollUp: true,
+      ticking: false,
+      showDetails: false
     };
   }
   componentDidMount() {
@@ -31,14 +45,19 @@ class ArticleView extends React.Component<Props, State> {
   }
   handleScroll() {
     if (
-      this.state.scrollPosition < window.scrollY - 50 ||
-      this.state.scrollPosition > window.scrollY + 50
+      (!this.state.ticking &&
+        this.state.scrollPosition < window.scrollY - 100) ||
+      this.state.scrollPosition > window.scrollY + 100
     ) {
-      this.setState({
-        scrollPosition: window.scrollY,
-        scrollUp:
-          window.scrollY < 20 || this.state.scrollPosition > window.scrollY
+      window.requestAnimationFrame(() => {
+        this.setState({
+          scrollPosition: window.scrollY,
+          scrollUp:
+            window.scrollY < 20 || this.state.scrollPosition > window.scrollY,
+          ticking: false
+        });
       });
+      this.setState({ ticking: true });
     }
   }
 
@@ -55,25 +74,32 @@ class ArticleView extends React.Component<Props, State> {
       ? article.metadata.has('siteName') || article.metadata.has('ogSiteName')
       : false;
 
-    const isWebkit = 'WebkitAppearance' in document.documentElement.style;
-
     return (
-      <Jumbotron>
-        {this.state.scrollUp && (
-          <Navbar
-            dark={true}
-            style={{
-              backgroundColor: '#33507f',
-              position: isWebkit ? '-webkit-sticky' : 'sticky',
-              top: '20px'
-            }}
-          >
+      <Jumbotron style={{ margin: '0 auto', width: '65vw' }}>
+        <Fade
+          className="article-list-bar"
+          style={{
+            backgroundColor: '#33507f',
+            top: '20px'
+          }}
+          in={this.state.scrollUp}
+        >
+          <Navbar dark={true}>
             <Nav navbar={true}>
               <NavItem>
                 <ExitArticleView />
               </NavItem>
             </Nav>
-            <NavbarBrand>
+            <NavbarBrand style={{ fontSize: '1vw', whiteSpace: 'pre-line' }}>
+              {hasSiteName
+                ? article.metadata.get('siteName') ||
+                  article.metadata.get('ogSiteName')
+                : ''}
+              {hasSiteName && hasDescription ? ' - ' : ''}
+              {hasDescription
+                ? article.metadata.get('ogDescrption') ||
+                  article.metadata.get('description')
+                : ''}
               {hasTitle
                 ? article.metadata.get('title') ||
                   article.metadata.get('ogTitle')
@@ -90,11 +116,49 @@ class ArticleView extends React.Component<Props, State> {
             </NavbarBrand>
             <Nav className="ml-auto" navbar={true}>
               <NavItem>
+                <Button
+                  size="sm"
+                  onClick={() =>
+                    this.setState({ showDetails: !this.state.showDetails })
+                  }
+                >
+                  <Icon name="info" />
+                </Button>
                 <ArticleMenu article={article} />
               </NavItem>
             </Nav>
           </Navbar>
-        )}
+          <Collapse isOpen={this.state.showDetails}>
+            {article.dateAdded ? (
+              <small>
+                Date added: {article.dateAdded.toLocaleDateString()} <br />
+              </small>
+            ) : (
+              ''
+            )}
+            {!article.viewedOn.isEmpty() ? (
+              <small>
+                {`Last viewed on ${article.viewedOn.last().toLocaleString()}
+                        - viewed ${article.viewedOn.size} time(s)`}
+                <br />
+              </small>
+            ) : (
+              ''
+            )}
+            {article.dateRead ? (
+              <small>
+                {'Date Read: ' + article.dateRead.toLocaleDateString()}
+                <br />
+              </small>
+            ) : (
+              ' '
+            )}
+            {article.projects
+              ? 'Projects: ' +
+                article.projects.map((t: string) => t + ' ').toJS()
+              : ' '}
+          </Collapse>
+        </Fade>
         {article.HTMLContent && <>{ReactHTMLParser(article.HTMLContent)}</>}
       </Jumbotron>
     );
