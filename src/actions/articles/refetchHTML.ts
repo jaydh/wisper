75ebea48f90bc593aa75ce5dc2214ba8a1040a7b
Jay Howard <jaydh@uw.edu>
2018-02-20 02:41:56 -0800

    Add listener for refetch that updates html

--- src/actions/articles/refetchHTML.ts
+++ src/actions/articles/refetchHTML.ts
@@ -1,12 +1,16 @@
 import { auth, database } from '../../firebase';
 import { Dispatch } from 'react-redux';
+import { setCurrentHTML } from '../ui/setCurrentArticle';
 
 export default function refetchHTML(id: string) {
   const user = auth().currentUser.uid;
   return (dispatch: Dispatch<any>) => {
-    const articleRef = database.ref(
-      `/userData/${user}/articles/${id}/refetch`
-    );
+    const articleRef = database.ref(`/userData/${user}/articles/${id}/refetch`);
+    database.ref(`/articleData/${id}/HTMLContent`).on('value', (data: any) => {
+      console.log(data.val());
+      dispatch(setCurrentHTML(data.val()));
+      database.ref(`/articleData/${id}/HTMLContent`).off();
+    });
     return articleRef.set(true);
   };
 }
