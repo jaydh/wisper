90025d5a33db8050b5122822a8169db743b12ee7
Jay Howard <jayhoward300@gmail.com>
2018-03-15 15:54:56 -0700

    Make refetch delete old db value before resetting for cloud trigger

--- src/actions/articles/refetchHTML.ts
+++ src/actions/articles/refetchHTML.ts
@@ -1,15 +1,9 @@
 import { database } from '../../firebase';
 import { Dispatch } from 'react-redux';
-import { setCurrentHTML } from '../ui/setCurrentArticle';
 
 export default function refetchHTML(id: string) {
   return (dispatch: Dispatch<any>) => {
     const articleRef = database.ref(`/articleData/${id}/refetch`);
-    database.ref(`/articleData/${id}/HTMLContent`).on('value', (data: any) => {
-      console.log(data.val());
-      dispatch(setCurrentHTML(data.val()));
-      database.ref(`/articleData/${id}/HTMLContent`).off();
-    });
-    return articleRef.set(true);
+    return articleRef.remove().then(() => articleRef.set(true));
   };
 }
