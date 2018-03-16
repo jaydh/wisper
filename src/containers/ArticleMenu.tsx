500a84df6ce5db11c943e9b72bea8409eaf366c2
Jay Howard <jayhoward300@gmail.com>
2018-03-16 15:51:31 -0700

    Add button in article menu to save individual articles for offline

--- src/containers/ArticleMenu.tsx
+++ src/containers/ArticleMenu.tsx
@@ -1,31 +1,58 @@
 import * as React from 'react';
-import ToggleArticle from '../containers/actionDispatchers/ToggleArticle';
-import DeleteArticle from '../containers/actionDispatchers/DeleteArticle';
-import { ButtonGroup, Button } from 'reactstrap';
+import ToggleArticle from './actionDispatchers/ToggleArticle';
+import DeleteArticle from './actionDispatchers/DeleteArticle';
+import { ButtonGroup, Button, UncontrolledTooltip } from 'reactstrap';
 import { Icon } from 'react-fa';
 import { Article } from '../constants/StoreState';
+import updateHTML from '../actions/articles/updateHTML';
+import { connect } from 'react-redux';
 
 interface Props {
   article: Article;
   collapseHandler?: () => void;
+  onSaveClick: () => void;
 }
 
-export default class ArticleMenu extends React.Component<Props> {
+class ArticleMenu extends React.Component<Props> {
   render() {
-    const { article, collapseHandler } = this.props;
+    const { article, collapseHandler, onSaveClick } = this.props;
     return (
       <ButtonGroup size="sm">
-        <Button onClick={() => window.open(article.link, '_blank')}>
+        <Button
+          id={`save${article.id}`}
+          onClick={() => (!article.HTMLContent ? onSaveClick() : null)}
+          disabled={article.HTMLContent}
+        >
+          <Icon name={!article.HTMLContent ? 'save' : 'check'} />
+        </Button>
+        <Button
+          id={`outside${article.id}`}
+          onClick={() => window.open(article.link, '_blank')}
+        >
           <Icon name="external-link" />
         </Button>
-        <ToggleArticle id={article.id} />
-        <DeleteArticle id={article.id} />
+        <ToggleArticle articleId={article.id} />
+        <DeleteArticle articleId={article.id} />
         {collapseHandler && (
           <Button onClick={collapseHandler}>
             <Icon name="plus" />
           </Button>
         )}
+        <UncontrolledTooltip placement="top" target={`save${article.id}`}>
+          {!article.HTMLContent ? <p>Save locally</p> : <p>Read for offline</p>}
+        </UncontrolledTooltip>{' '}
+        <UncontrolledTooltip placement="top" target={`outside${article.id}`}>
+          Go to full website
+        </UncontrolledTooltip>
       </ButtonGroup>
     );
   }
 }
+
+const mapDispatchToProps = (dispatch: any, ownProps: any) => {
+  return {
+    onSaveClick: () => dispatch(updateHTML(ownProps.article.id))
+  };
+};
+
+export default connect(null, mapDispatchToProps)(ArticleMenu);
