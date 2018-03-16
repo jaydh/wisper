7eec389367adb6903f2f529356fc99c83d02771f
Jay Howard <jayhoward300@gmail.com>
2018-03-16 13:59:59 -0700

    Add listeneres when refetch occurs

--- src/actions/articles/refetchHTML.ts
+++ src/actions/articles/refetchHTML.ts
@@ -1,9 +1,29 @@
 import { database } from '../../firebase';
-import { Dispatch } from 'react-redux';
-
+import updateHTML from './updateHTML';
+import updateMetadata from './updateMetadata';
+import updateFetching from './updateFetching';
 export default function refetchHTML(id: string) {
-  return (dispatch: Dispatch<any>) => {
-    const articleRef = database.ref(`/articleData/${id}/refetch`);
-    return articleRef.remove().then(() => articleRef.set(true));
+  return dispatch => {
+    const articleRef = database.ref(`/articleData/${id}`);
+    dispatch(updateFetching(id, true));
+    // Trigger cloud function that listens for write on refetch
+    articleRef
+      .child('refetch')
+      .remove()
+      .then(() => articleRef.child('refetch').set(true));
+    articleRef
+      .child('HTMLContent')
+      .on('value', (snap: any) => dispatch(updateHTML(id)));
+    articleRef
+      .child('metadata')
+      .on('value', (snap: any) => dispatch(updateMetadata(id, snap.val())));
+    articleRef.child('fetching').on('value', (snap: any) => {
+      // If done fetching turn off listeners
+      if (snap.val() === false) {
+        articleRef.child('HTMLContent').off();
+        articleRef.child('metadata').off();
+      }
+      dispatch(updateFetching(id, snap.val()));
+    });
   };
 }
