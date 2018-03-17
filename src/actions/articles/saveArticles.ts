980e81f5341a699bac57c9deb33e847ec6fd6fc6
Jay Howard <jayhoward300@gmail.com>
2018-03-17 15:40:47 -0700

    Remove log statements

--- src/actions/articles/saveArticles.ts
+++ src/actions/articles/saveArticles.ts
@@ -5,14 +5,12 @@ import { Dispatch } from 'react-redux';
 export default function saveArticles() {
   const user = auth()!.currentUser!.uid;
   const articleRef = database.ref('/userData/' + user + '/articles/');
-  console.log('call');
   return (dispatch: Dispatch<any>) =>
     articleRef
       .orderByChild('completed')
       .equalTo(false)
       .once('value')
       .then(function(snap: any) {
-        console.log('one');
         let articles = snap.val();
         for (const key in articles) {
           if (key) {
