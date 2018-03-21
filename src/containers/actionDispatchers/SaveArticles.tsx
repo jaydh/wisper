0ee65571d616c76bfbcf662e0c9f545d90f21966
Jay Howard <jayhoward300@gmail.com>
2018-03-20 23:13:55 -0700

    Move save articles logic to container that downloads visible articles

--- src/containers/actionDispatchers/SaveArticles.tsx
+++ src/containers/actionDispatchers/SaveArticles.tsx
@@ -1,29 +1,38 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import saveArticles from '../../actions/articles/saveArticles';
-import { Button } from 'reactstrap';
+import updateHTML from '../../actions/articles/updateHTML';
+import { Button, UncontrolledTooltip } from 'reactstrap';
 import { Icon } from 'react-fa';
+import { Set } from 'immutable';
+import { Article } from '../../constants/StoreState';
 
-export interface Props {
+interface Props {
   onSaveClick: () => void;
+  articles: Set<Article>;
 }
 
-class SaveArticle extends React.Component<Props, {}> {
+class SaveArticle extends React.Component<Props> {
   render() {
     const { onSaveClick } = this.props;
     return (
-      <Button onClick={() => onSaveClick()}>
-        <Icon name="save" />
-      </Button>
+      <>
+        <Button id="saveArticles" onClick={() => onSaveClick()}>
+          <Icon name="save" />
+        </Button>
+        <UncontrolledTooltip placement="bottom" target="saveArticles">
+          Save current articles
+        </UncontrolledTooltip>
+      </>
     );
   }
 }
 
 const mapDispatchToProps = (dispatch: any, ownProps: any) => {
   return {
-    onSaveClick: () => {
-      dispatch(saveArticles());
-    }
+    onSaveClick: () =>
+      ownProps.articles.map(
+        (t: Article) => (!t.HTMLContent ? dispatch(updateHTML(t.id)) : null)
+      )
   };
 };
 
