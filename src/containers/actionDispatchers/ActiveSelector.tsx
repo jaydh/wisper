2a725dbcdc4a703e76ba42c01708337c072b58a9
Jay Howard <jayhoward300@gmail.com>
2018-04-09 16:50:04 -0700

    Change active selector to change on click and have separate dropdown button

--- src/containers/actionDispatchers/ActiveSelector.tsx
+++ src/containers/actionDispatchers/ActiveSelector.tsx
@@ -3,16 +3,20 @@ import * as React from 'react';
 import { setVisibilityFilter } from '../../actions/ui/visibilityFilter';
 import { pullCompletedArticles } from '../../actions/syncWithFirebase';
 import {
+  Button,
+  ButtonGroup,
   ButtonDropdown,
   DropdownMenu,
   DropdownToggle,
   DropdownItem
 } from 'reactstrap';
+import { Article } from '../../constants/StoreState';
 
 interface Props {
   onClick: (t: string) => void;
   fetchCompleted: () => void;
   currentActive: string;
+  emptyCompleted: boolean;
 }
 interface State {
   isOpen: Boolean;
@@ -28,34 +32,46 @@ class ActiveSelector extends React.Component<Props, State> {
     this.setState({ isOpen: !this.state.isOpen });
   }
   render() {
-    const { onClick, currentActive } = this.props;
+    const { onClick, currentActive, emptyCompleted } = this.props;
     const options = ['Active', 'Completed', 'Today', 'All'];
+    const nextActive =
+      options[
+        options.indexOf(currentActive) === options.length - 1
+          ? 0
+          : options.indexOf(currentActive) + 1
+      ];
+    if (
+      (currentActive === 'Completed' || currentActive === 'All') &&
+      emptyCompleted
+    ) {
+      this.props.fetchCompleted();
+    }
     return (
-      <ButtonDropdown isOpen={this.state.isOpen} toggle={this.toggle}>
-        <DropdownToggle caret={true}>{currentActive}</DropdownToggle>
-        <DropdownMenu>
-          {options.map((t: string) => (
-            <DropdownItem
-              key={'activeSelector' + t}
-              onClick={() => {
-                onClick(t);
-                if (t === 'Completed' || t === 'All') {
-                  this.props.fetchCompleted();
-                }
-              }}
-            >
-              {t}
-            </DropdownItem>
-          ))}
-        </DropdownMenu>
-      </ButtonDropdown>
+      <ButtonGroup>
+        <ButtonDropdown isOpen={this.state.isOpen} toggle={this.toggle}>
+          <Button onClick={() => onClick(nextActive)}>{currentActive}</Button>
+          <DropdownToggle caret={true} />
+          <DropdownMenu>
+            {options.map((t: string) => (
+              <DropdownItem
+                key={'activeSelector' + t}
+                onClick={() => onClick(t)}
+              >
+                {t}
+              </DropdownItem>
+            ))}
+          </DropdownMenu>
+        </ButtonDropdown>
+      </ButtonGroup>
     );
   }
 }
 
 const mapStateToProps = (state: any, ownProps: any) => {
   return {
-    currentActive: state.get('articleLists').visibilityFilter
+    currentActive: state.get('articleLists').visibilityFilter,
+    emptyCompleted:
+      state.get('articles').filter((t: Article) => t.completed).size === 0
   };
 };
 
