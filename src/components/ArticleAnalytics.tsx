22a41a457f985dfcd30f5420601beabf72d5e723
Jay Howard <jayhoward300@gmail.com>
2018-04-09 16:49:02 -0700

    Add completed article pulling for analytics

--- src/components/ArticleAnalytics.tsx
+++ src/components/ArticleAnalytics.tsx
@@ -2,7 +2,17 @@ import * as React from 'react';
 import ProjectsGraphs from '../containers/graphs/ProjectsGraph';
 import SourcesGraphs from '../containers/graphs/SourcesGraph';
 import { Container } from 'reactstrap';
-class ArticleAnalytics extends React.Component {
+import { pullCompletedArticles } from '../actions/syncWithFirebase';
+import { connect } from 'react-redux';
+
+interface Props {
+  fetch: () => void;
+}
+
+class ArticleAnalytics extends React.Component<Props> {
+  componentDidMount() {
+    this.props.fetch();
+  }
   render() {
     return (
       <Container>
@@ -13,4 +23,10 @@ class ArticleAnalytics extends React.Component {
   }
 }
 
-export default ArticleAnalytics;
+const mapDispatchToProps = dispatch => {
+  return {
+    fetch: () => dispatch(pullCompletedArticles())
+  };
+};
+
+export default connect(null, mapDispatchToProps)(ArticleAnalytics);
